ad4003cc67e1c82dd2cb825e865ba1cb70592e20
priya <laxmisuprini@gmail.com>
2019-04-30 14:09:52 +0530

    update

--- supervised/pima.py
+++ supervised/pima.py
@@ -1,117 +1,229 @@
-import csv 
-import numpy as np 
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
-import warnings
-warnings.filterwarnings('ignore') 
-  
-def loadCSV(filename): 
-    ''' 
-    function to load dataset 
-    '''
-    with open(filename,"r") as csvfile: 
-        lines = csv.reader(csvfile) 
-        dataset = list(lines) 
-        for i in range(len(dataset)): 
-            dataset[i] = [float(x) for x in dataset[i]]      
-    return np.array(dataset) 
-  
-  
-def normalize(X): 
-    ''' 
-    function to normalize feature matrix, X 
-    '''
-    mins = np.min(X, axis = 0) 
-    maxs = np.max(X, axis = 0) 
-    rng = maxs - mins 
-    norm_X = 1 - ((maxs - X)/rng) 
-    return norm_X 
-  
-  
-def logistic_func(beta, X): 
-    ''' 
-    logistic(sigmoid) function 
-    '''
-    return 1.0/(1 + np.exp(-np.dot(X, beta.T))) 
-  
-  
-def log_gradient(beta, X, y): 
-    ''' 
-    logistic gradient function 
-    '''
-    first_calc = logistic_func(beta, X) - y.reshape(X.shape[0], -1) 
-    final_calc = np.dot(first_calc.T, X) 
-    return final_calc 
-  
-  
-def cost_func(beta, X, y): 
-    ''' 
-    cost function, J 
-    '''
-    log_func_v = logistic_func(beta, X) 
-    y = np.squeeze(y) 
-    step1 = y * np.log(log_func_v) 
-    step2 = (1 - y) * np.log(1 - log_func_v) 
-    final = -step1 - step2 
-    return np.mean(final) 
-  
-  
-def grad_desc(X, y, beta, lr=.01, converge_change=.001): 
-    ''' 
-    gradient descent function 
-    '''
-    cost = cost_func(beta, X, y) 
-    change_cost = 1
-    num_iter = 1
-      
-    while(change_cost > converge_change): 
-        old_cost = cost 
-        beta = beta - (lr * log_gradient(beta, X, y)) 
-        cost = cost_func(beta, X, y) 
-        change_cost = old_cost - cost 
-        num_iter += 1
-      
-    return beta, num_iter  
-  
-  
-def pred_values(beta, X): 
-    ''' 
-    function to predict labels 
-    '''
-    pred_prob = logistic_func(beta, X) 
-    pred_value = np.where(pred_prob >= .5, 1, 0) 
-    return np.squeeze(pred_value) 
-  
-      
-if __name__ == "__main__": 
-    # load the dataset 
-    dataset = loadCSV('pima-indians-diabetes.data.csv') 
-      
-    # normalizing feature matrix 
-    X = normalize(dataset[:, :-1]) 
-      
-    # stacking columns wth all ones in feature matrix 
-    X = np.hstack((np.matrix(np.ones(X.shape[0])).T, X)) 
-  
-    # response vector 
-    y = dataset[:, -1] 
-    test_size=0.33
-    seed=7
-    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=test_size,random_state=seed)
-  
-    # initial beta values 
-    beta = np.matrix(np.zeros(X.shape[1])) 
-  
-    # beta values after running gradient descent 
-    beta, num_iter = grad_desc(X, y, beta) 
-        
-    # predicted labels 
-    y_pred = pred_values(beta, X_test) 
-      
-    # number of correctly predicted labels 
-    print("Correctly predicted labels:", np.sum(y_test == y_pred)) 
-    print ('Accuracy:', accuracy_score(y_test,y_pred))
-    print(classification_report(y_test,y_pred))
-    
-    
+from random import seed
+from random import randrange
+from csv import reader
+from math import sqrt
+from sklearn.metrics import precision_score ,recall_score, f1_score
+ 
+# Load a CSV file
+def load_csv(filename):
+	dataset = list()
+	with open(filename, 'r') as file:
+		csv_reader = reader(file)
+		for row in csv_reader:
+			if not row:
+				continue
+			dataset.append(row)
+	return dataset
+ 
+# Convert string column to float
+def str_column_to_float(dataset, column):
+	for row in dataset:
+		row[column] = float(row[column].strip())
+ 
+# Convert string column to integer
+def str_column_to_int(dataset, column):
+	class_values = [row[column] for row in dataset]
+	unique = set(class_values)
+	lookup = dict()
+	for i, value in enumerate(unique):
+		lookup[value] = i
+	for row in dataset:
+		row[column] = lookup[row[column]]
+	return lookup
+ 
+# Split a dataset into k folds
+def cross_validation_split(dataset, n_folds):
+	dataset_split = list()
+	dataset_copy = list(dataset)
+	fold_size = int(len(dataset) / n_folds)
+	for i in range(n_folds):
+		fold = list()
+		while len(fold) < fold_size:
+			index = randrange(len(dataset_copy))
+			fold.append(dataset_copy.pop(index))
+		dataset_split.append(fold)
+	return dataset_split
+ 
+# Calculate accuracy percentage
+def accuracy_metric(actual, predicted):
+	correct = 0
+	for i in range(len(actual)):
+		if actual[i] == predicted[i]:
+			correct += 1
+	return correct / float(len(actual)) * 100.0
+ 
+# Evaluate an algorithm using a cross validation split
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+	folds = cross_validation_split(dataset, n_folds)
+	scores = list()
+	pre = list()
+	recall=list()
+	f1 =list()
+	for fold in folds:
+		train_set = list(folds)
+		train_set.remove(fold)
+		train_set = sum(train_set, [])
+		test_set = list()
+		for row in fold:
+			row_copy = list(row)
+			test_set.append(row_copy)
+			row_copy[-1] = None
+		predicted = algorithm(train_set, test_set, *args)
+		actual = [row[-1] for row in fold]
+		accuracy = accuracy_metric(actual, predicted)
+		precision = precision_score(actual,predicted)
+		recalls   = recall_score(actual,predicted)
+		F1 = recall_score(actual,predicted)
+		scores.append(accuracy)
+		pre.append(precision)
+		recall.append(recalls)
+		f1.append(F1)
+
+	return scores,pre,recall,f1
+ 
+# Split a dataset based on an attribute and an attribute value
+def test_split(index, value, dataset):
+	left, right = list(), list()
+	for row in dataset:
+		if row[index] < value:
+			left.append(row)
+		else:
+			right.append(row)
+	return left, right
+ 
+# Calculate the Gini index for a split dataset
+def gini_index(groups, classes):
+	# count all samples at split point
+	n_instances = float(sum([len(group) for group in groups]))
+	# sum weighted Gini index for each group
+	gini = 0.0
+	for group in groups:
+		size = float(len(group))
+		# avoid divide by zero
+		if size == 0:
+			continue
+		score = 0.0
+		# score the group based on the score for each class
+		for class_val in classes:
+			p = [row[-1] for row in group].count(class_val) / size
+			score += p * p
+		# weight the group score by its relative size
+		gini += (1.0 - score) * (size / n_instances)
+	return gini
+ 
+# Select the best split point for a dataset
+def get_split(dataset, n_features):
+	class_values = list(set(row[-1] for row in dataset))
+	b_index, b_value, b_score, b_groups = 999, 999, 999, None
+	features = list()
+	while len(features) < n_features:
+		index = randrange(len(dataset[0])-1)
+		if index not in features:
+			features.append(index)
+	for index in features:
+		for row in dataset:
+			groups = test_split(index, row[index], dataset)
+			gini = gini_index(groups, class_values)
+			if gini < b_score:
+				b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+	return {'index':b_index, 'value':b_value, 'groups':b_groups}
+ 
+# Create a terminal node value
+def to_terminal(group):
+	outcomes = [row[-1] for row in group]
+	return max(set(outcomes), key=outcomes.count)
+ 
+# Create child splits for a node or make terminal
+def split(node, max_depth, min_size, n_features, depth):
+	left, right = node['groups']
+	del(node['groups'])
+	# check for a no split
+	if not left or not right:
+		node['left'] = node['right'] = to_terminal(left + right)
+		return
+	# check for max depth
+	if depth >= max_depth:
+		node['left'], node['right'] = to_terminal(left), to_terminal(right)
+		return
+	# process left child
+	if len(left) <= min_size:
+		node['left'] = to_terminal(left)
+	else:
+		node['left'] = get_split(left, n_features)
+		split(node['left'], max_depth, min_size, n_features, depth+1)
+	# process right child
+	if len(right) <= min_size:
+		node['right'] = to_terminal(right)
+	else:
+		node['right'] = get_split(right, n_features)
+		split(node['right'], max_depth, min_size, n_features, depth+1)
+ 
+# Build a decision tree
+def build_tree(train, max_depth, min_size, n_features):
+	root = get_split(train, n_features)
+	split(root, max_depth, min_size, n_features, 1)
+	return root
+ 
+# Make a prediction with a decision tree
+def predict(node, row):
+	if row[node['index']] < node['value']:
+		if isinstance(node['left'], dict):
+			return predict(node['left'], row)
+		else:
+			return node['left']
+	else:
+		if isinstance(node['right'], dict):
+			return predict(node['right'], row)
+		else:
+			return node['right']
+ 
+# Create a random subsample from the dataset with replacement
+def subsample(dataset, ratio):
+	sample = list()
+	n_sample = round(len(dataset) * ratio)
+	while len(sample) < n_sample:
+		index = randrange(len(dataset))
+		sample.append(dataset[index])
+	return sample
+ 
+# Make a prediction with a list of bagged trees
+def bagging_predict(trees, row):
+	predictions = [predict(tree, row) for tree in trees]
+	return max(set(predictions), key=predictions.count)
+ 
+# Random Forest Algorithm
+def random_forest(train, test, max_depth, min_size, sample_size, n_trees, n_features):
+	trees = list()
+	for i in range(n_trees):
+		sample = subsample(train, sample_size)
+		tree = build_tree(sample, max_depth, min_size, n_features)
+		trees.append(tree)
+	predictions = [bagging_predict(trees, row) for row in test]
+	return(predictions)
+ 
+# Test the random forest algorithm
+seed(2)
+# load and prepare data
+filename = 'pima-indians-diabetes.data.csv'
+dataset = load_csv(filename)
+# convert string attributes to integers
+for i in range(0, len(dataset[0])-1):
+	str_column_to_float(dataset, i)
+# convert class column to integers
+str_column_to_int(dataset, len(dataset[0])-1)
+# evaluate algorithm
+n_folds = 5
+max_depth = 10
+min_size = 1
+sample_size = 1.0
+n_features = int(sqrt(len(dataset[0])-1))
+for n_trees in [1, 5, 10]:
+	scores = evaluate_algorithm(dataset, random_forest, n_folds, max_depth, min_size, sample_size, n_trees, n_features)
+	print('Trees: %d' % n_trees)
+	#print('Scores: %s' % scores[0])
+	print('Accuracy: %.3f%%' % (sum(scores[0])/float(len(scores[0]))))
+	print('precision:%.3f%%'%(sum(scores[1])/float(len(scores[1]))*100))
+	print('recall:%.3f%%'%(sum(scores[2])/float(len(scores[2]))*100))
+	print('f1:%.3f%%'%(sum(scores[3])/float(len(scores[3]))*100))
+
+
